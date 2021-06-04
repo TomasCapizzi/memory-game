c613263c6615482a7fb5649cf13d0ca013eea375
TomasCapizzi <tomas.capizzi@hotmail.com>
2021-06-03 21:29:38 -0300

    Corrección Fallo doble click en misma img

--- codigo.js
+++ codigo.js
@@ -1,74 +1,80 @@
 const btnPlay = document.getElementById('play');
 
 const cartas = [{
-        codigo: 1,
+        codigo: 1,        
         id: 1,
         nombre: 'oceano',
         src: 'https://images.unsplash.com/photo-1468581264429-2548ef9eb732?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 2,
         id: 2,
+        estado: false,
         nombre: 'bosque',
         src: 'https://images.unsplash.com/photo-1582566517828-a5eb582a76d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=963&q=80'
     }, {
         codigo: 3,
         id: 3,
+        estado: false,
         nombre: 'city',
         src: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1113&q=80'
     }, {
         codigo: 4,
         id: 4,
+        estado: false,
         nombre: 'cerro',
         src: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 5,
         id: 5,
+        estado: false,
         nombre: 'motaña',
         src: 'https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 6,
         id: 6,
+        estado: false,
         nombre: 'playa',
         src: 'https://images.unsplash.com/photo-1468413253725-0d5181091126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 7,
         id: 1,
+        estado: false,
         nombre: 'oceano',
         src: 'https://images.unsplash.com/photo-1468581264429-2548ef9eb732?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 8,
         id: 2,
+        estado: false,
         nombre: 'bosque',
         src: 'https://images.unsplash.com/photo-1582566517828-a5eb582a76d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=963&q=80'
     }, {
         codigo: 9,
         id: 3,
+        estado: false,
         nombre: 'city',
         src: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1113&q=80'
     }, {
         codigo: 10,
         id: 4,
+        estado: false,
         nombre: 'cerro',
         src: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 11,
         id: 5,
+        estado: false,
         nombre: 'motaña',
         src: 'https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
     }, {
         codigo: 12,
         id: 6,
+        estado: false,
         nombre: 'playa',
         src: 'https://images.unsplash.com/photo-1468413253725-0d5181091126?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
     }
 
 ]
 
-const fondo = {
-    id: 0,
-    nombre: 'fondo',
-    src: 'https://i.pinimg.com/originals/93/b5/f9/93b5f9913d2e4316cd6e541c67b9aed0.jpg'
-}
 
 
 
@@ -105,41 +111,44 @@ function crearTablero() {
         carta.src = cartas[i].src;
         contenedorImg.appendChild(carta)
         tablero.appendChild(contenedorImg);
+        let imgClickeada = false;
         setTimeout(() => {
             carta.addEventListener('click', () => { // evento click al clickear imagen
                 carta.style.opacity = 1; //Muestro la imagen
-                guardarElección(cartas[i]); //Guardamo eleccion
+                guardarElección(cartas[i]); //Guardamos eleccion                
+                
                 if ((eleccion.length % 2) == 0 && eleccion.length > 1) {
-                    controlarResultado(eleccion[0].id, eleccion[1].id, carta);
-                } //Controlo los resultados en nros pares, osea al segundo click
-
-                if (eleccion[0]) {
-
-                }
-
+                    controlarResultado(eleccion[0].id, eleccion[1].id,eleccion[0].codigo, eleccion[1].codigo ,imgClickeada, carta);
+                } //Controlo los resultados en nros pares, osea al segundo click          
+                imgClickeada = true;
             });
-        }, 3200)
+        }, 3300)
 
         // Funcion que oculta las imágenes luego de 2 segundos, dado que es el tiempo inicial de observación del tablero antes de adivinar
         setTimeout(() => {
             opacidad(carta);
             contenedorImg.style.cursor = 'pointer';
             resultado.innerText = ''; // Quito la advertencia
-        }, 2500);
+        }, 3300);
     }
 
 }
 
 
-function controlarResultado(valor1, valor2, carta) {
-    // console.log(valor1, valor2)
-
-    if (opcionesCorrectas < 5) { // Ejecuta hasta antes de adivinar la última
-        if (valor1 == valor2) { // Si los valores elegidos son iguales
+function controlarResultado(valor1, valor2, codigo1, codigo2,estado,carta) {
+    if(estado){
+        console.log('No vale!');
+        return eleccion=[];
+    }else if (opcionesCorrectas < 5) { // Ejecuta hasta antes de adivinar la última
+        if (valor1 == valor2 && codigo1 != codigo2) { // Si los valores elegidos son iguales
             opcionesCorrectas++; //Suma la opcion
             console.log('Correcto', opcionesCorrectas);
             return eleccion = []; //Borra el array
-        } else if (vidasRestantes == 2) { //En cambio si no son correctas
+        }else if(codigo1 == codigo2){
+            console.log('No puedes clickear la misma imagen');
+            return eleccion.splice(1,1)
+        } 
+        else if (vidasRestantes == 2) { //En cambio si no son correctas
             console.log('Incorrecto');
             descontarVida(vida1)
             resultado.innerText = 'Te queda 1 vida'; // Adviere que queda 1 sola vida
@@ -206,7 +215,7 @@ function gameOver() {
 
 
 function opacidad(element) {
-    element.style.opacity = 0;
+    element.style.opacity = 0.2;
 }
 
 
